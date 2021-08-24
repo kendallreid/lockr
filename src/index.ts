2b0a08e313237ec8b2cf1274947afc5cafe274a7
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2021-08-24 12:24:37 +0300

    Expose all function in a default export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-export { default as set } from './set';
-export { default as sadd } from './sadd';
-export { default as smembers } from './smembers';
-export { default as rm } from './rm';
-export { default as get } from './get';
-export { default as getAll } from './getAll';
-export { default as flush } from './flush';
-export { default as keys } from './keys';
-export { default as sismember } from './sismember';
-export { default as srem } from './srem';
-export * from './prefix';
+import set from './set';
+import sadd from './sadd';
+import smembers from './smembers';
+import rm from './rm';
+import get from './get';
+import getAll from './getAll';
+import flush from './flush';
+import keys from './keys';
+import sismember from './sismember';
+import srem from './srem';
+import { getPrefix, hasPrefix, setPrefix, getPrefixedKey } from './prefix';
+
+export {
+	set,
+	sadd,
+	smembers,
+	rm,
+	get,
+	getAll,
+	flush,
+	keys,
+	sismember,
+	srem,
+	getPrefixedKey,
+	getPrefix,
+	setPrefix,
+	hasPrefix,
+}
